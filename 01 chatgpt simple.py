89c46672470bebae955cf585cfd6c1b0127236b4
matthew813709 <104387212+matthew813709@users.noreply.github.com>
2023-08-17 05:20:41 -0400

    Delete 01 chatgpt simple.py

--- 01 chatgpt simple.py
+++
@@ -1,6 +0,0 @@
-import openai
-
-openai.api_key = "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"
-
-completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "write an essay on penguins "}])
-print(completion.choices[0].message.content)
